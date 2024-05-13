5549f0d9e3159c164ed1b4f733ef3edf623d2998
mohcine1789 <moxchine1789@gmail.com>
2024-05-13 10:50:36 +0200

    reformatting

--- src/test/java/restassured/BaseTest.java
+++ src/test/java/restassured/BaseTest.java
@@ -12,12 +12,12 @@ public abstract class BaseTest {
 
     protected static Dotenv dotEnv = Dotenv.configure().load();
     protected static final String API_KEY = dotEnv.get("API_KEY");
-    private static final String BASE_URL =  dotEnv.get("BASE_URL");
+    private static final String BASE_URL = dotEnv.get("BASE_URL");
     protected static final RequestSpecification requestSpecification = new RequestSpecBuilder()
             .setBaseUri(BASE_URL)
             .setContentType(ContentType.JSON)
             .addQueryParam("apikey", API_KEY)
-            .build();;
+            .build();
 
     @BeforeClass
     void setUp() {
--- src/test/java/restassured/StockMarketApiTest.java
+++ src/test/java/restassured/StockMarketApiTest.java
@@ -4,29 +4,29 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
-
 import java.io.InputStream;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-public class RestApiTest extends BaseTest {
+public class StockMarketApiTest extends BaseTest {
+
+    private static String TIME_SERIES_DAILY_FUNCTION = "TIME_SERIES_DAILY";
 
     @Test
     void testGetStockSuccessfully_function_TIME_SERIES_DAIL() {
         String IBMSymbol = "IBM";
-        String DailyFunction = "TIME_SERIES_DAILY";
         String symbolJsonPath = "'Meta Data'.'2. Symbol'";
-        InputStream getStockJsonSchema = getClass ().getClassLoader ()
-                .getResourceAsStream ("json-schemas/get-stock-validator.json");
+        InputStream getStockJsonSchema = getClass().getClassLoader()
+                .getResourceAsStream("json-schemas/get-stock-validator.json");
 
         given()
                 .spec(requestSpecification)
-        .when()
-                .queryParam("function", DailyFunction)
+                .when()
+                .queryParam("function", TIME_SERIES_DAILY_FUNCTION)
                 .queryParam("symbol", IBMSymbol)
                 .get("query")
-        .then()
+                .then()
                 .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .body(JsonSchemaValidator.matchesJsonSchema(getStockJsonSchema))
@@ -36,18 +36,18 @@ public class RestApiTest extends BaseTest {
 
     @Test
     void testGetStockWithError_apikey_missing() {
-        String symbol = "IBM";
-        String function = "TIME_SERIES_DAILY";
+        String MBGSymbol = "MBG.DEX";
+        String emptyApiKey = "";
         String missingApikeyErrorMessage = "the parameter apikey is invalid or missing";
 
         given()
                 .spec(requestSpecification)
-        .when()
-                .queryParam("function", function)
-                .queryParam("symbol", symbol)
-                .queryParam("apikey", "")
+                .when()
+                .queryParam("function", TIME_SERIES_DAILY_FUNCTION)
+                .queryParam("symbol", MBGSymbol)
+                .queryParam("apikey", emptyApiKey)
                 .get("query")
-        .then()
+                .then()
                 .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .time(lessThan(1000L))
